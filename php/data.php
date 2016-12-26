86df240a52fd1f3ff41a116f123f4c8efebf7c87
zhangxy <yueyue_6301@163.com>
2016-12-26 15:23:22 +0800

    “修改js

--- php/data.php
+++ php/data.php
@@ -1,11 +1,12 @@
 <?php
+
 if($_GET['num']==0){
-	echo $_REQUEST['callback'].'(' . json_encode('第一项的内容") . ')';
+	echo $_REQUEST['callback'].'(' . json_encode('第一项的内容') . ')';
 }
 else if($_GET['num']==1){
-	echo $_REQUEST['callback'].'(' . json_encode("第二项的内容") . ')';
+	echo $_REQUEST['callback'].'(' . json_encode('第二项的内容') . ')';
 }
 else if($_GET['num']==2){
-	echo $_REQUEST['callback'].'(' . json_encode("第三项的内容") . ')';
+	echo $_REQUEST['callback'].'(' . json_encode('第三项的内容') . ')';
 }
 ?> 
