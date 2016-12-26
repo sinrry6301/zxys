4126eec61bf6e9651e947aec64ce3d709ce310dd
zhangxy <yueyue_6301@163.com>
2016-12-26 14:43:34 +0800

    ““修改js

--- php/data.php
+++ php/data.php
@@ -1,13 +1,13 @@
 <?php
 
 if($_GET['num']==0){
-	echo $_REQUEST['callback'].'第一项的内容';
+	echo $_REQUEST['callback']."("第一项的内容")";
 }
 else if($_GET['num']==1){
-	echo $_REQUEST['callback'].'第二项的内容';
+	echo $_REQUEST['callback']."("第一项的内容")";
 }
 else if($_GET['num']==2){
-	echo $_REQUEST['callback'].'第三项的内容';
+	echo $_REQUEST['callback']."("第一项的内容")";
 }
 
 ?> 
